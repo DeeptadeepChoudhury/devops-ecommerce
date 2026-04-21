ecc7dd2386245d5d055c92a0784c2b0847b01ac0
rio <choudhury.rio@gmail.com>
2026-04-21 10:24:55 +0000

    Ports fixed

--- services/product-service/src/main/java/com/devops/product_service/controller/ProductController.java
+++ services/product-service/src/main/java/com/devops/product_service/controller/ProductController.java
@@ -1,18 +1,18 @@
 package com.devops.product_service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Product Service is running";
+    @GetMapping
+    public String getProducts() {
+        return "List of products";
     }
 
-    @GetMapping("/products")
-    public String products() {
-        return "List of products";
+    @PostMapping
+    public String createProduct() {
+        return "Product created";
     }
 }
